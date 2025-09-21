0651de6339aeda43a57386274ac4e097876e5a28
A-Daksh <136158651+AarushiDaksh@users.noreply.github.com>
2025-09-21 15:09:16 +0530

    Initial commit

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.use("/api/mappings", mappingRoutes);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
